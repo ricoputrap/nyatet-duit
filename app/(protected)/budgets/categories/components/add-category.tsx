21765ceacd9ca3ffe4ab6d8f05185532a1ec0f0d
Rico Putra Pradana <ricopradanaputra2198@gmail.com>
2025-11-02 15:27:19 +0700

    feat: simplify CreateCategoryButton by removing form handling logic

--- app/(protected)/budgets/categories/components/add-category.tsx
+++ app/(protected)/budgets/categories/components/add-category.tsx
@@ -1,29 +1,14 @@
 "use client"
 
-import React from "react"
-import { toast } from "sonner"
 import { Plus } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import useFormSheetStore from "@/components/form-sheet/store"
 
-import { createCategory } from "../actions"
 import CategoryForm from "./form"
 
-
 export function CreateCategoryButton() {
-  const { open, close } = useFormSheetStore()
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const formData = new FormData(e.currentTarget)
-    const name = formData.get('name') as string
-
-    await createCategory(name);
-    toast.success('Category created successfully');
-
-    // Close the form sheet after submission
-    close();
-  };
+  const open = useFormSheetStore(state => state.open)
 
   return (
     <Button onClick={() => open("Create Category", <CategoryForm />)}>
